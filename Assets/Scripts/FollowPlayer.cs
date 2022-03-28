ee8677e5508be319df6c9187da7d8900bb3efe00
Jackson Scollan <js02777@srcs.org>
2022-03-28 15:51:28 -0700

    Tutorial 1.3 Complete

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public GameObject Player;
+    //Offset the camera behind the player by adding to players position
+    private Vector3 offset = new Vector3(0, 6, -8);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //Sets position of camera to position of players current position
+        transform.position = Player.transform.position + offset;
+    }
+}
