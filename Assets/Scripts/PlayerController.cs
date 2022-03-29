235eca92ef9178e77ab85a23b7a638b1f3848459
Jackson Scollan <js02777@srcs.org>
2022-03-29 14:29:51 -0700

    Tutorial 1.4.2 Completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     // Sets speed of vehicle
     public float speed = 10.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
