324e9b983488f0f3d5f38bcd299db17990aac6a7
Jackson Scollan <js02777@srcs.org>
2022-05-24 14:54:55 -0700

    Day 2 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 5.0f;
-
-    public GameObject projectilePrefab;
+    public bool gameOver = false;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -32,11 +32,7 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // Launch a projectile from the player
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        }
+       
     }
 }
 
